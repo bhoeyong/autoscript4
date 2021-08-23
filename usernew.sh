80a26e4cb135ca6fa2591d07973fd4b2c40b61bd
Renzel Belgado <30442976+darkrenz@users.noreply.github.com>
2021-08-23 14:39:22 +0800

    Update usernew.sh

--- usernew.sh
+++ usernew.sh
@@ -5,7 +5,6 @@ read -p "Password : " Pass
 read -p "Expired (hari): " masaaktif
 
 IP=$(wget -qO- icanhazip.com);
-domain=$(cat /etc/v2ray/domain);
 echo Script AutoCreate Akun SSH dan OpenVPN Mod By TuanYz
 sleep 1
 echo Ping Host
@@ -24,25 +23,19 @@ exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
 echo -e ""
 echo -e "Terima Kasih Telah Menggunakan Layanan Kami"
-echo -e "Informasi Akun SSH & OpenVPN"
+echo -e "Account Information WS-SSH & WS-SSL/TLS"
 echo -e "Username          : $Login "
 echo -e "Password          : $Pass"
 echo -e "==============================="
 echo -e "IP Server         : $IP"
-echo -e "Host              : $domain"
+echo -e ""
 echo -e "OpenSSH           : 22, 500"
-echo -e "Dropbear          : 143, 109"
-echo -e "WS-SSH            : 80 ,8880"
+echo -e "WS-SSH            : 80"
 echo -e "WS-SSH SSL/TLS    : 443"
-echo -e "SSL/TLS           : 444"
 echo -e "BadVPN/UDPGW      : 7100, 7200, 7300"
-echo -e "Squid             : 3128, 8080 (limit to IP SSH)" 
-echo -e "OpenVPN           : TCP 1194 http://$IP:81/client-tcp-1194.ovpn"
-echo -e "OpenVPN           : UDP 2200 http://$IP:81/client-udp-2200.ovpn"
-echo -e "OpenVPN           : SSL 992 http://$IP:81/client-tcp-ssl.ovpn"
 echo -e "==============================="
-echo -e "Aktif Sampai      : $exp"
+echo -e "Actil Until      : $exp"
 echo -e "==============================="
 echo -e "Payload Default WS:"
 echo -e "GET / HTTP/1.1[crlf]Host: $domain[crlf]Connection: Keep-Alive[crlf]User-Agent: [ua][crlf]Upgrade: websocket[crlf][crlf]"
-echo -e "Script Mod By TuanYz"
+echo -e "Modify by. PHCORNER / N O T E"
