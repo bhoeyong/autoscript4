472a57794af388d9784c65c6d8834f3d231ae2fe
Renzel Belgado <30442976+darkrenz@users.noreply.github.com>
2021-08-23 21:17:29 +0800

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -38,7 +38,7 @@ echo -e "$merah 16$putih. Info Script Auto Install $biru(about)"
 echo -e "$merah 17$putih. Customize your banner $biru(banner)"
 echo -e 
 echo -e ""
-echo -e "$cyan Untuk keluar dari menu tekan$merah CTRL + C"
+echo -e "$cyan To exit the menu press$merah CTRL + C"
 echo -e "$putih==============================================================="
 read -rp " Please Enter The Number [1-60] : " -e num
 if [[ "$num" = "1" ]]; then
