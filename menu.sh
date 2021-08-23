3e65e998e29c6ba29396f5f8b8de38bb6940c38c
Renzel Belgado <30442976+darkrenz@users.noreply.github.com>
2021-08-23 21:15:58 +0800

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#Menu
 merah="\e[1;31m"
 kuning="\e[1;33m"
 biru="\e[1;34m"
@@ -18,24 +19,55 @@ echo -e "$kuning ━━━━━━━━━━━━━━$kuning OpenSSH & SSH
 echo -e "$merah  1$putih. Create SSH & SSL Account $biru(usernew)"
 echo -e "$merah  2$putih. Generate SSH & SSL Trial Account $biru(trial)"
 echo -e "$merah  3$putih. Extending SSH & SSL Account Active Life $biru(renew)"
-echo -e "$merah  4$putih. Delete SSH & SSL Account $biru(delete)"
-echo -e "$merah  5$putih. Check User Login SSH & OpenVPN $biru(wipeout)"
-echo -e "$merah  7$putih. Delete User Expired SSH & OpenVPN $biru(delete)"
-echo -e "$merah  8$putih. Set up Autokill SSH $biru(autokill)"
-echo -e "$merah  9$putih. Displays Users Who Do Multi Login SSH $biru(ceklim)"
+echo -e "$merah  4$putih. Delete SSH & SSL Account $biru(wipeout)"
+echo -e "$merah  5$putih. Delete User Expired SSH & OpenVPN $biru(delete)"
+echo -e "$merah  6$putih. Set up Autokill SSH $biru(autokill)"
+echo -e "$merah  7$putih. Displays Users Who Do Multi Login SSH $biru(ceklim)"
 echo -e "$putih━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
 echo -e 
 echo -e "$kuning ━━━━━━━━━━━━━━━━$kuning SYSTEM━━━━━━━━━━━━━━━━"
-echo -e "$merah 11$putih. Restart service SSH/SSL $biru(restart)"
-echo -e "$merah 12$putih. Reboot VPS $biru(reboot)"
-echo -e "$merah 13$putih. Speedtest VPS $biru(speedtest)"
-echo -e "$merah 14$putih. Display system information $biru(info)"
-echo -e "$merah 15$putih. Info script auto install $biru(about)"
-echo -e "$merah 16$putih. Exit $biru(exit)"
-echo -e "$merah 17$putih. Check Usage of VPS Ram $biru(ram)"
-echo -e "$merah 18$putih. Reboot VPS $biru(reboot)"
-echo -e "$merah 19$putih. Speedtest VPS $biru(speedtest)"
-echo -e "$merah 20$putih. Displaying System Information $biru(info)"
-echo -e "$merah 21$putih. Info Script Auto Install $biru(about)"
-echo -e "$merah 22$putih. Customize your banner $biru(banner)"
+echo -e "$merah 8$putih. Restart service SSH/SSL $biru(restart)"
+echo -e "$merah 9$putih. Reboot VPS $biru(reboot)"
+echo -e "$merah 10$putih. Speedtest VPS $biru(speedtest)"
+echo -e "$merah 11$putih. Display system information $biru(info)"
+echo -e "$merah 12$putih. Info script auto install $biru(about)"
+echo -e "$merah 13$putih. Exit $biru(exit)"
+echo -e "$merah 14$putih. Speedtest VPS $biru(speedtest)"
+echo -e "$merah 15$putih. Displaying System Information $biru(info)"
+echo -e "$merah 16$putih. Info Script Auto Install $biru(about)"
+echo -e "$merah 17$putih. Customize your banner $biru(banner)"
 echo -e 
+echo -e ""
+echo -e "$cyan Untuk keluar dari menu tekan$merah CTRL + C"
+echo -e "$putih==============================================================="
+read -rp " Please Enter The Number [1-60] : " -e num
+if [[ "$num" = "1" ]]; then
+usernew
+elif [[ "$num" = "2" ]]; then
+trial
+elif [[ "$num" = "3" ]]; then
+renew
+elif [[ "$num" = "4" ]]; then
+wipeout
+elif [[ "$num" = "5" ]]; then
+delete
+elif [[ "$num" = "6" ]]; then
+autokill
+elif [[ "$num" = "7" ]]; then
+ceklim
+elif [[ "$num" = "8" ]]; then
+restart
+elif [[ "$num" = "9" ]]; then
+reboot
+elif [[ "$num" = "10" ]]; then
+speedtest
+elif [[ "$num" = "11" ]]; then
+info
+elif [[ "$num" = "12" ]]; then
+about
+elif [[ "$num" = "13" ]]; then
+banner
+else
+clear
+menu
+fi
