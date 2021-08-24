f2410bf00e76b747d0c717f670cc497a26d9731f
Renzel Belgado <30442976+darkrenz@users.noreply.github.com>
2021-08-24 16:57:19 +0800

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -82,7 +82,7 @@ sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
 apt-get --reinstall --fix-missing install -y bzip2 gzip coreutils wget screen rsyslog iftop htop net-tools zip unzip wget net-tools curl nano sed screen gnupg gnupg1 bc apt-transport-https build-essential dirmngr libxml-parser-perl neofetch git
 echo "clear" >> .profile
 echo "neofetch" >> .profile
-echo "echo \e[32mJUST TYPE : MENU" >> .profile
+echo "echo JUST TYPE : MENU" >> .profile
 
 # install badvpn
 cd
