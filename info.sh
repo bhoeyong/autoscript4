f763e59211823af5f222d6317700fe6a7a92f284
Renzel Belgado <30442976+darkrenz@users.noreply.github.com>
2021-08-24 13:49:22 +0800

    Create info.sh

---
+++ info.sh
@@ -0,0 +1,18 @@
+echo "━━━━━━━━━━━━━━━━━━━━━━━🄸🄽🄵🄾🅁🄼🄰🅃🄸🄾🄽━━━━━━━━━━━━━━━━━" | tee -a log-install.txt
+echo ""
+echo "✕✕✕✕✕✕✕✕✕✕✕✕✕✕✕✕✕✕✕✕✕✕✕✕✕✕✕✕✕✕✕✕✕✕✕✕✕✕✕✕✕✕✕✕✕✕" | tee -a log-install.txt
+echo ""
+echo "   🅂🄴🅁🅅🄸🄲🄴 🄰🄽🄳 🄿🄾🅁🅃"  | tee -a log-install.txt
+echo "   - OpenSSH                 : 22"  | tee -a log-install.txt
+echo "   - SSH(Websocket)          : 80"  | tee -a log-install.txt
+echo "   - SSL/TLS(WS)             : 443"  | tee -a log-install.txt
+echo "   - Badvpn                  : 7100, 7200, 7300"  | tee -a log-install.txt
+echo "   - Nginx                   : 81"
+echo ""
+echo "   🅂🄴🅁🅅🄴🅁 🄸🄽🄵🄾🅁🄼🄰🅃🄸🄾🄽 & 🄾🅃🄷🄴🅁 🄵🄴🄰🅃🅄🅁🄴🅂"
+echo ""
+echo "   - Timezone                : Asia/Jakarta (GMT +7)"
+echo "   - Auto-Reboot             : [ON]"
+echo "   - Autoreboot Time         : 00.00 GMT +7"
+echo ""
+echo "━━━━━━━━━━━━━━━━━━━🅼🅾🅳🅸🅵🅸🅴🅳 🅱🆈 🅽🅾🆃🅴━━━━━━━━━━━━━━" | tee -a log-install.txt
