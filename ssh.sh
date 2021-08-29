c8d0a13438fbd623ecd185fbf037883c7fff5c84
Renzel Belgado <30442976+darkrenz@users.noreply.github.com>
2021-08-29 19:44:10 +0800

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -101,6 +101,7 @@ apt-get -y update
 cd
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g'
 # /etc/ssh/sshd_config
+sed -i '/Port 22/a Port 456' /etc/ssh/sshd_config
 sed -i '/Port 22/a Port 500' /etc/ssh/sshd_config
 sed -i '/Port 22/a Port 40000' /etc/ssh/sshd_config
 sed -i '/Port 22/a Port 51443' /etc/ssh/sshd_config
